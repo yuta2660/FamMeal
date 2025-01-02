9cf660e9a4cfb3b0502fb565f5461534de9ef92c
yuta2660 <imamu0511yuta@gmail.com>
2025-01-03 00:28:12 +0900

    初回コミット

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* ville作成ページ */}
+        {/* <Route path="/" element={<Grid />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
